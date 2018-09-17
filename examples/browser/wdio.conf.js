fee2bb2863e42031a37bc19f6091e6e1fc47db51
Simon Marchi <simon.marchi@ericsson.com>
2018-09-17 10:02:40 -0400

    Run examples/browser UI tests using a temporary workspace
    I am looking to add a UI test for some C/C++ features.  That will
    require writing some files to the workspace and open them in the editor.
    Right now, the workspace used for these tests seems to be based on the
    content of ~/.theia/recentworkspace.json.  We obviously don't want to
    start writing stuff in the user's real workspaces, and since we don't
    know what we will find in there, it could make the tests unstable.
    
    This patch makes the UI tests run with a temporary workspace, deleted
    after the test run.
    
    All the UI tests still run simultaneously using the same
    workspace/backend, so there is still room for unpredictability.
    
    Signed-off-by: Simon Marchi <simon.marchi@ericsson.com>

--- examples/browser/wdio.conf.js
+++ examples/browser/wdio.conf.js
@@ -1,8 +1,13 @@
 // @ts-check 
 const http = require('http');
 const path = require('path');
+const temp = require('temp');
 
 const wdioRunnerScript = require.resolve('webdriverio/build/lib/runner.js');
+
+// Remove .track() if you'd like to keep the workspace and other temporary
+// files after running the tests.
+const temptrack = temp.track();
 
 /**
  * WebdriverIO will execute this current script first to setup the tests,
@@ -183,9 +188,15 @@ exports.config = {
     // resolved to continue.
     //
     // Gets executed once before all workers get launched.
-    onPrepare: function (config, capabilities) {
+    onPrepare: function(config, capabilities) {
+        // Modify process.argv so that the server (which is in the
+        // master process) starts with a temporary directory as the
+        // workspace.
+        const rootDir = temptrack.mkdirSync();
+        process.argv.push('--root-dir', rootDir);
         return require('./src-gen/backend/server')(port, host).then(created => {
-            this.execArgv = [wdioRunnerScript, cliPortKey, created.address().port];
+            this.execArgv = [wdioRunnerScript, cliPortKey, created.address().port,
+                '--theia-root-dir', rootDir];
             this.server = created;
         });
     },
